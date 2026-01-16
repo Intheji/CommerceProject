6bb746b24d27f8e978428165d9300d8dbcb3e885
hyunji <143073470+Intheji@users.noreply.github.com>
2026-01-16 13:13:50 +0900

    step1 - feat: 상품 데이터 리스트 추가

--- src/main/java/com/example/commerce/Main.java
+++ src/main/java/com/example/commerce/Main.java
@@ -1,7 +1,22 @@
 package com.example.commerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        // 프로그램 시작 출력
         System.out.println("[ 실시간 커머스 플랫폼 - 전자제품]");
+
+        // 여러 Product를 담기 위한 List 생성
+        List<Product> products = new ArrayList<>();
+
+        // Product 객체 생성을 하고 List에 추가한다
+        // new Produc()를 이용해서 실제 상품 객체를 생성한다
+        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 50));
+        products.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 30));
+        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 20));
+        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 100));
     }
 }
