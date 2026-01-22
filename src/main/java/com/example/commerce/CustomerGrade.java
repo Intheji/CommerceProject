bee8a6f5ce0002c02ae7f4ff951a1a7140c68ea5
hyunji <143073470+Intheji@users.noreply.github.com>
2026-01-22 20:31:48 +0900

    challenge3 - feat: 고객 등급 enum 만듦

---
+++ src/main/java/com/example/commerce/CustomerGrade.java
@@ -0,0 +1,30 @@
+package com.example.commerce;
+
+// 고객 등급
+public enum CustomerGrade {
+
+    BRONZE(0, "브론즈"),
+    SLIVER(5, "실버"),
+    GOLD(10, "골드"),
+    PLATINUM(15, "플래티넘");
+
+    private final  int discountPercentage;
+    private final String description;
+
+    CustomerGrade(int discountPercentage, String description) {
+        this.discountPercentage = discountPercentage;
+        this.description = description;
+    }
+
+    public int getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long calculateDiscount(long amount) {
+        return amount * discountPercentage / 100;
+    }
+}
