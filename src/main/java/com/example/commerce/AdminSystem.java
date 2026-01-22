721c93321f4184120a8ce075d8704fc2c17fa88e
hyunji <143073470+Intheji@users.noreply.github.com>
2026-01-22 14:42:44 +0900

    challenge2 - feat: AdminSystem에서 상품 추가 기능 구현

--- src/main/java/com/example/commerce/AdminSystem.java
+++ src/main/java/com/example/commerce/AdminSystem.java
@@ -1,5 +1,6 @@
 package com.example.commerce;
 
+import javax.swing.text.Utilities;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,8 +33,72 @@ public class AdminSystem {
             System.out.println("0. 메인으로 돌아가기");
 
             int adminChoice = InputUtil.readInt(sc, "선택>");
+
+            if (adminChoice == 0) {
+                System.out.println("메인으로 돌아갑니다.");
+                return;
+            }
+
+            if (adminChoice == 1) {
+                addProduct(sc);
+            } else {
+                System.out.println();
+            }
+
         }
 
+    }
+
+    private void addProduct(Scanner sc) {
+
+        Category category = selectCategory(sc);
+
+        if (category == null) {
+            System.out.println("상품 추가를 취소했습니다.");
+            return;
+        }
+
+        System.out.println("\n[상품 추가]");
+
+        System.out.println("상품명: ");
+        String name = sc.nextLine();
+
+        int price = InputUtil.readInt(sc, "가격: ");
+
+        System.out.println("설명: ");
+        String description = sc.nextLine();
+
+        int stock = InputUtil.readInt(sc, "재고: ");
+
+        Product product = new Product(name, price, description, stock);
+        category.addProduct(product);
+
+        System.out.println("상품이 추가되었습니다");
+    }
+
+    private Category selectCategory(Scanner sc) {
+        System.out.println("\n[카테고리 선택]");
+
+        for (int i = 0; i < this.categories.size(); i++) {
+            int menuNumber = i + 1;
+            System.out.println(menuNumber + ". " + categories.get(i).getName());
+        }
+
+        System.out.println("0. 취소");
+
+        int choice = InputUtil.readInt(sc, "선택> ");
+
+        if (choice == 0) {
+            return null;
+        }
+
+        int index = choice - 1;
+
+        if (index < 0 || index >= this.categories.size()) {
+            System.out.println("잘못된 번호입니다.");
+            return null;
+        }
+        return this.categories.get(index);
     }
 
 }
