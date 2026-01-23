17119a69f1e79b4012664ba83aa7b6a51e7118d2
hyunji <143073470+Intheji@users.noreply.github.com>
2026-01-23 12:35:47 +0900

    challenge3 - feat: 의류/식품 카테고리 상품 데이터 추가

--- src/main/java/com/example/commerce/Main.java
+++ src/main/java/com/example/commerce/Main.java
@@ -14,16 +14,28 @@ public class Main {
         electronicsProducts.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 20));
         electronicsProducts.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 100));
 
+
         // 의류 카테고리용 상품 리스트 만들기
         List<Product> clothingProducts = new ArrayList<>();
+        clothingProducts.add(new Product("롱패딩", 300000, "겨울 필수 김밥",15));
+        clothingProducts.add(new Product("후드티", 59000, "질 좋은 후드티!",50));
+        clothingProducts.add(new Product("청바지", 79000, "기모가 들어있는 따뜻한 청바지",25));
+        clothingProducts.add(new Product("운동화", 159000, "아주 편한 운동화!",30));
+
 
         // 식품 카테고리용 상품 리스트 만들기
         List<Product> foodProducts = new ArrayList<>();
+        foodProducts.add(new Product("치킨", 21000, "반반치킨", 100));
+        foodProducts.add(new Product("라면", 4500, "틈새라면! 매운데 맛있어~", 300));
+        foodProducts.add(new Product("삼겹살", 15000, "싸다 싸 삼겹살", 30));
+        foodProducts.add(new Product("우유", 2900, "저지방 우유", 140));
+
 
         // Category 객체 만듦
         Category electronics = new Category("전자제품", electronicsProducts);
         Category clothing = new Category("의류", clothingProducts);
         Category food = new Category("식품", foodProducts);
+
 
         List<Category> categories = new ArrayList<>();
         categories.add(electronics);
